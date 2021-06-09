4fb4808012efade1309078134e56674e649212bb
wleskay <wleskay@email.wm.edu>
2021-06-09 10:41:01 -0500

    lifting state up

--- src/components/Coin/Coin.jsx
+++ src/components/Coin/Coin.jsx
@@ -11,42 +11,27 @@ const Td = styled.td `
 export default class Coin extends Component {
     constructor(props){
         super(props);
-        this.state = {
-            price: this.props.price
-        }
         this.handleClick = this.handleClick.bind(this);
     }
-    //Use this code if the page needs a constant refresh.
-    /*componentDidMount(){
-        const callback = () => {
-            //set the state to a new random value 
-            const randomPercentage = 0.995 + Math.random() * 0.01;
-
-            this.setState(function(oldstate){
-                return{
-                    price: oldstate.price * randomPercentage
-                };
-            });
-        }
-        setInterval(callback, 1000);
-    }*/
     handleClick(event){
         //Prevent the default action of submitting the form
         event.preventDefault();
-        const randomPercentage = 0.995 + Math.random() * 0.01;
 
+        this.props.handleRefresh(this.props.ticker);
+
+        /*const randomPercentage = 0.995 + Math.random() * 0.01;
         this.setState(function(oldstate){
             return{
                 price: oldstate.price * randomPercentage
             };
-        });
+        });*/
     }
     render() {
         return(
             <tr>
               <Td>{this.props.name}</Td>
               <Td>{this.props.ticker}</Td>
-              <Td>${this.state.price}</Td>
+              <Td>${this.props.price}</Td>
               <Td>
                 <form action = "#" method = "POST">
                   <button onClick={this.handleClick}>Refresh</button>
